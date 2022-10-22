4e7c8a480bed848d1a9f227c28b6e91e596d6f65
daniel cespedes <daniel@DanielC-PC>
2022-10-22 08:57:54 -0500

    FIX webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: 'CursoPractico-React-JS/bundle.js',
         publicPath: './',
         clean: true,
     },
